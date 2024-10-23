fead637eec80925106c2d55594434ee1e2f8fb96
axeasemay <axeasemay@alu.edu.gva.es>
2024-10-23 09:59:33 +0200

    Tercero de calc

--- app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcCalculatorActivity.kt
@@ -3,11 +3,24 @@ package com.example.imcapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+    private fun initComponents()
+    {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+    private fun initListeners()
+    {
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
+        initComponents()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_imc_calculator)
